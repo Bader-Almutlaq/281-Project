29f0bf234603bc7cd034dd20b8e2e9e03367f23e
Bader Almutlaq <badr11almutlaq@gmail.com>
2023-10-18 15:53:26 +0300

    Adding comments and changing variables names

--- EuclideanAlgorithm.java
+++ EuclideanAlgorithm.java
@@ -1,18 +1,24 @@
 public class EuclideanAlgorithm {
-    private static LinkedStack<Integer> numbers;
+    private static LinkedStack<Integer> numbers; // a Stack to store the equations.
 
     public static int calculateGCD(int a, int b) {
-        if (numbers == null)
+        if (numbers == null) // If the stack was not created.
             numbers = new LinkedStack<Integer>();
 
-        emptyStack();
+        emptyStack(); // Empty the stack from the previous operations.
 
-        while (b != 0) {
+        if (b > a) { // Swapping if the second number is bigger.
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        while (b != 0) { // Going through the loop until the remainder equals zero
             int quotient = a / b;
             int remainder = a % b;
             System.out.println(a + " = " + quotient + " x " + b + " + " + remainder);
 
-            if (remainder != 0) {
+            if (remainder != 0) { // Filing the stack but not including the last equation
                 numbers.push(a);
                 numbers.push(quotient);
                 numbers.push(b);
@@ -26,50 +32,57 @@ public class EuclideanAlgorithm {
     }
 
     public static int[] calculateLinearCombination(int num1, int num2) {
-        if (numbers == null || numbers.empty())
+        if (numbers == null || numbers.empty()) // If the stack is not created or empty we call calculateGCD().
             calculateGCD(num1, num2);
 
-        int arr[] = new int[2];
+        int arr[] = new int[2]; // Array that stores the result.
 
-        int gcd = numbers.pop();
+        // Popping the numbers in order to work backward
+        int gcd = numbers.pop(); // GCD was the last number added to the stack.
         int y = numbers.pop();
-        int qy = numbers.pop();
+        int y_Coefficient = numbers.pop();
         int x = numbers.pop();
-        int qx = 1;
-        System.out.println(gcd + " = " + x + " - " + qy + " x " + y);
+        int x_Coefficient = 1; // Coefficient of x is always 1 in the first equation.
 
-        while (!numbers.empty()) {
-            int tempY = numbers.pop();
-            int z3 = numbers.pop();
+        System.out.println(gcd + " = " + x + " - " + y_Coefficient + " x " + y); // Printing the first equation.
+
+        while (!numbers.empty()) { // Going through the loop to print the equations until the stack is empty.
+
+            int tempY = numbers.pop(); // The remainder in the equation
+            int z3 = numbers.pop(); // These numbers will substitute the remainder
             int z2 = numbers.pop();
             int z1 = numbers.pop();
 
-            if (tempY == y) {
+            if (tempY == y) { // If the remainder is equal to y we substitute in y.
                 System.out
                         .println(
-                                gcd + " = " + qx + " x " + x + " - " + qy + " x " + " ( " + z1 + " - " + z2 + " x " + z3
+                                gcd + " = " + x_Coefficient + " x " + x + " - " + y_Coefficient + " x " + " ( " + z1
+                                        + " - " + z2 + " x " + z3
                                         + " )");
-                qx += (qy * z2);
-                y = z1;
-                System.out.println(gcd + " = " + qx + " x " + x + " - " + qy + " x " + y);
 
-            } else {
+                x_Coefficient += (y_Coefficient * z2); // multiply Coefficient
+                y = z1; // prepare for the next equation.
+                System.out.println(gcd + " = " + x_Coefficient + " x " + x + " - " + y_Coefficient + " x " + y);
+
+            } else { // substitute in x.
                 System.out
-                        .println(gcd + " = " + qx + " x " + " ( " + z1 + " - " + z2 + " x " + z3 + " )" + " - " + qy
+                        .println(gcd + " = " + x_Coefficient + " x " + " ( " + z1 + " - " + z2 + " x " + z3 + " )"
+                                + " - " + y_Coefficient
                                 + " x " + y);
-                qy += (qx * z2);
-                x = z1;
-                System.out.println(gcd + " = " + qx + " x " + x + " - " + qy + " x " + y);
+
+                y_Coefficient += (x_Coefficient * z2); // multiply Coefficient
+                x = z1; // prepare for the next equation.
+                System.out.println(gcd + " = " + x_Coefficient + " x " + x + " - " + y_Coefficient + " x " + y);
             }
         }
 
-        arr[0] = qx;
-        arr[1] = -qy;
+        arr[0] = x_Coefficient;
+        arr[1] = -y_Coefficient;
         return arr;
     }
 
     private static void emptyStack() {
-        while (!numbers.empty())
+        while (!numbers.empty()) // Pop all the numbers in the stack.
             numbers.pop();
     }
 }
